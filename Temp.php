7a36cb7a3c510a1cd815d7d7940557794c418606
Arkadiusz Adach <adach_soft@wp4b.pl>
2017-12-25 10:38:14 +0100

    f3

--- Temp.php
+++ Temp.php
@@ -13,10 +13,92 @@
 class Temp
 {
 
-	public static function getSensors()
+	const FILE_LIST_OF_DEVICES = '/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves';
+
+	private $sensors = [];
+	private $mapSensors = [];
+	private $lastTimeExec = false;
+
+	/**
+	 * Construct for class
+	 */
+	public function __construct()
 	{
-		$str = file_get_contents('/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves');
-		$dev_ds18b20 = preg_split("/\\r\\n|\\r|\\n/", $str);
-		var_dump($dev_ds18b20);
+		$this->getSensors();
+	}
+
+	public function getLastTimeExec()
+	{
+		return $this->lastTimeExec;
+	}
+
+	/**
+	 * Get all devices
+	 */
+	public function getSensors()
+	{
+		$str = file_get_contents(static::FILE_LIST_OF_DEVICES);
+		$devs = preg_split("/\\r\\n|\\r|\\n/", $str);
+
+		$this->sensors = [];
+		foreach ($devs as $key => $val) {
+			if (!empty($val)) {
+				$this->sensors[] = $val;
+			}
+		}
+
+		return $this->sensors;
+	}
+
+	public function setMap($key, $devId)
+	{
+		if (isset($this->mapSensors[$key])) {
+			$this->throwException('zvxcd');
+		}
+		$this->mapSensors[$key] = $devId;
+	}
+
+	public function getDeviceId($key)
+	{
+		if (preg_match("/^[0-9]$/i", $key)) {
+			if (!isset($this->sensors[$key])) {
+				$this->throwException('Unknown device: ' . $key);
+			}
+			return $this->sensors[$key];
+		} elseif (in_array($key, $this->sensors)) {
+			return $key;
+		}
+
+		return $this->mapSensors[$key];
+	}
+
+	/**
+	 * Read the temperature of the device in degrees Celsius
+	 * @param int|string $sensor
+	 * @return float
+	 */
+	public function readTemp($sensor)
+	{
+		$timeStart = microtime(true);
+		$this->lastTiemExec = false;
+		$dev = $this->getDeviceId($sensor);
+
+		$temp = 0;
+		$temp_path = "/sys/bus/w1/devices/$dev/w1_slave";
+		$str = file_get_contents($temp_path);
+		if (preg_match('|t\=([0-9]+)|mi', $str, $m)) {
+			$temp = (float) ($m[1] / 1000);
+		} else {
+			return false;
+		}
+
+		$timeEnd = microtime(true);
+		$this->lastTimeExec = $timeEnd - $timeStart;
+		return $temp;
+	}
+
+	protected function throwException($message)
+	{
+		throw new Exception($message);
 	}
 }
